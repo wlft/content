0b593b91e717ba8472cfb23c7cc5f2ab8a81351b
Wolfite <100052562+WWolfite@users.noreply.github.com>
2023-04-20 13:47:03 +1000

    Create DisableDefaultHP.lua

---
+++ rss/DisableDefaultHP.lua
@@ -0,0 +1,10 @@
+--[[
+	RSS, Roblox Studio Simplified
+	
+	DisableDefaultHP.lua
+	
+	Written by @Wolfite_
+	21 April, 2023
+]]
+
+game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.Health, false)
